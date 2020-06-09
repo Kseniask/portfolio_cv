06cf1f6283278b2166281f9564437205a36a3b27
kseniask <kseniask30@icloud.com>
2020-06-09 16:44:07 -0700

    skills scroll available

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 $(function () {
+  // $('.bxslider').bxSlider({
+  //   displaySlideQty: 3
+  //   // moveSlideQty: 1
+  // })
+  $(window).scroll = function () {
+    sticky_nav()
+  }
+  var navbar = $('.nav')
+  var sticky = navbar.offsetTop
+
+  function sticky_nav () {
+    if (window.pageYOffset >= sticky) {
+      navbar.addClass('sticky')
+    } else if (window.pageYOffset <= sticky && navbar.hasClass('sticky')) {
+      navbar.removeClass('sticky')
+    }
+  }
+
+  $('.horiz_scroll').hover(
+    function () {
+      $(this).css('cursor', 'pointer')
+      $('.more').hide()
+    },
+    function () {
+      $('.more').show()
+    }
+  )
+  $('.horiz_scroll').mousewheel(function (e, delta) {
+    this.scrollLeft += delta * 20
+    e.preventDefault()
+  })
+
   var traits = $('.rectangle')
   $('.trait_text').hide()
 
@@ -6,7 +38,6 @@ $(function () {
     function () {
       var desc = $(this).next()
       var dots = $(this).find('.dots')
-      console.log(desc)
       desc.show()
       dots.hide()
     },
@@ -16,6 +47,15 @@ $(function () {
       dots.show()
       desc.hide()
       //   dots.hide()
+    }
+  )
+  $('.trait_text').hover(
+    function () {
+      $(this).show()
+      $(this).css('margin-top', '-10px')
+    },
+    function () {
+      $(this).hide()
     }
   )
 })
